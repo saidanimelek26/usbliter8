a6ddf0a1c0c7eae6d9b85b427634172154e49785
melek saidani <mbcmelek3@gmail.com>
2026-07-20 03:28:22 -0700

    Extend OLED API: add message/done/error functions

--- pico_oled/oled.h
+++ pico_oled/oled.h
@@ -7,5 +7,8 @@
 void oled_init_i2c(i2c_inst_t *i2c, uint gpio_sda, uint gpio_scl);
 void oled_show_status(bool connected);
 void oled_show_connecting(void);
+void oled_show_message(const char *line1, const char *line2);
+void oled_show_done(void);
+void oled_show_error(void);
 
 #endif // OLED_H
